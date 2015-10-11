033f57d6a9736877894415263cf0911743abff2a
cwelke <cwelke@cern.ch>
2015-10-11 06:20:11 -0700

    update version

--- ZMET2015/settings.sh
+++ ZMET2015/settings.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # export analysis_version="V00-00-17"
-export analysis_version="V07-04-10"
+export analysis_version="V07-04-11"
 echo "Analysis version = $analysis_version"
 localdirectory=`pwd`
 
